40d157e9485e9acd54a624d7ac83f684e321efe0
astel <kiyomillefeuille@gmail.com>
2018-09-26 19:14:45 +0900

    amazonのurlだけに反応するように

--- build.sbt
+++ build.sbt
@@ -8,7 +8,9 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   "com.typesafe" % "config" % "1.3.3",
-  "com.github.gilbertw1" %% "slack-scala-client" % "0.2.3"
+  "com.github.gilbertw1" %% "slack-scala-client" % "0.2.3",
+  "ch.qos.logback" % "logback-classic" % "1.2.1",
+  "org.slf4j" % "slf4j-api" % "1.7.12"
 )
 
 enablePlugins(JavaAppPackaging)
--- src/main/scala/net/astail/Main.scala
+++ src/main/scala/net/astail/Main.scala
@@ -1,14 +1,21 @@
 package net.astail
 
 import java.net.{HttpURLConnection, URL}
+
 import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
+import org.slf4j.{Logger, LoggerFactory}
 import slack.rtm.SlackRtmClient
+
 import scala.util.Random
 
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val logger: Logger = LoggerFactory.getLogger(this.getClass)
+
+    logger.info("start app")
+
 
     val token = ConfigFactory.load.getString("slack_token")
     val botChannel = ConfigFactory.load.getString("slack_channel")
@@ -23,22 +30,32 @@ object Main {
       val channelId: String = message.channel
       val channel = client.state.getChannelIdForName(botChannel).getOrElse("")
       val user = client.state.getUserById(message.user).map(_.name).getOrElse("")
+      val text = message.text
 
-      val sendMessage = logCheck(message.text, user)
+      val sendMessageOption = logCheck(text, user)
 
-
-      // todo: [warn]なのでどうにかする
-      channelId match {
-        case channel => client.sendMessage(channel, sendMessage)
-        case _ =>
+      sendMessageOption match {
+        case Some(s) => {
+          channelId match {
+            case channel => {
+              logger.info("================================================================")
+              logger.info(s"text: $text")
+              logger.info(s"sendMessage: $s")
+              logger.info("================================================================")
+              client.sendMessage(channel, s.toString)
+            }
+            case _ =>
+          }
+        }
+        case None =>
       }
     }
   }
 
 
-  def logCheck(message: String, user: String): String = {
+  def logCheck(message: String, user: String): Option[String] = {
     message match {
-      case s if s contains ("pubg") => s"@$user fps"
+      case s if s contains ("pubg") => Some(s"@$user fps")
       case _ => url2affi(message, user)
     }
   }
@@ -47,37 +64,43 @@ object Main {
   val userList = ConfigFactory.load.getString("affi_user_list")
 
   // todo: tryで包む
-  val setUserMap: Map[String,String] =
+  val setUserMap: Map[String, String] =
     userList.split(",").map(_.replaceAll("\\(|\\)", "").trim).toList.map(_.split("=")).map(xs => xs(0) -> xs(1)).toMap
 
-  val userMap: Map[String,String] =
+  val userMap: Map[String, String] =
     Map((setUserMap.toList(0)._1, setUserMap.toList(1)._2), (setUserMap.toList(1)._1, setUserMap.toList(0)._2))
 
 
   def r = new Random().nextInt(userMap.size)
 
-  def url2affi(message: String, user: String): String = {
+  def url2affi(message: String, user: String): Option[String] = {
     val tag = userMap.get(user) match {
       case Some(v) => v
       case _ => userMap.toList(r)._2
     }
 
-
-    // todo: url以外に反応しない
-
     // slackのurlは< >に囲まれているので先頭と行末を消す
     val url = message drop 1 dropRight 1
 
-    // 短縮urlを展開
-    HttpURLConnection setFollowRedirects false
-    val openUrl = new URL(url) openConnection() getHeaderField ("Location")
 
-    // もともとついてあるtag移行の文字を消す
-    val deleteTagUrl = openUrl match {
-      case s if s contains ("&tag=") => s replaceAll("&tag=.*", "")
-      case _ => openUrl
+    if (url.startsWith("https://www.amazon.co.jp") || url.startsWith("https://amzn.to")) {
+
+      // 短縮urlを展開
+      HttpURLConnection setFollowRedirects false
+      val openUrl = new URL(url) openConnection() getHeaderField ("Location")
+
+      // もともとついてあるtag移行の文字を消す
+      val deleteTagUrl = openUrl match {
+        case s if s contains ("&tag=") => s replaceAll("&tag=.*", "")
+        case _ => openUrl
+      }
+
+      Some("@" + user + " " + deleteTagUrl + "/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=" + tag)
+
+    } else {
+      None
     }
 
-    "@" + user + " " + deleteTagUrl + "/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=" + tag
   }
+
 }
